5062412ac57a757cac109e69cd220cb3f1f62c93
Piola Evania <piolaevania@gmail.com>
2023-09-14 18:22:53 +0800

    Add files via upload

---
+++ PraktikumPemrograman2/src/PRAKTIKUM1/PRAK004_2210817220018_PiolaEvania.java
@@ -0,0 +1,12 @@
+package PRAKTIKUM1;
+
+public class PRAK004_2210817220018_PiolaEvania {
+
+	public static void main(String[] args) {
+		String Nama = "Piola Evania";
+		int Umur = 19;
+		
+		System.out.println("Perkenalkan, Nama Saya "+ Nama +", Umur Saya "+ Umur);
+	}
+
+}
