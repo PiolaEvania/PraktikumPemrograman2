182c8186edb6aa52c300bad3cec2f0894ca9978e
Piola Evania <piolaevania@gmail.com>
2023-12-12 20:19:37 +0800

    Delete PRAKTIKUM6 directory

--- PRAKTIKUM6/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.praktikum6 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.praktikum6 to javafx.fxml;
-    exports org.example.praktikum6;
-}
